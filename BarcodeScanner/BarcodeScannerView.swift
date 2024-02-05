9ebc6d81f35926edbd4fb807d992990b065c987c
Bruno Alves <alves.bruno945@gmail.com>
2024-02-04 23:47:05 -0300

    Change `ContentView` name to `BarcodeScannerView `

---
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BarcodeScanner
+//
+//  Created by Bruno Silva on 04/02/24.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
