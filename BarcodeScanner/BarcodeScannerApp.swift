9a1561223d64012ca50e417b7ba748cc6afd1fa3
Bruno Alves <alves.bruno945@gmail.com>
2024-02-04 23:41:15 -0300

    Add initial commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Bruno Silva on 04/02/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
