a9d504cf8a0491740c82c3ec9e7fd0e117ba8f5a
Bruno Alves <alves.bruno945@gmail.com>
2024-02-15 22:49:25 -0300

    Initial setup from `ScannerViewController`

--- BarcodeScanner/ScannerViewController.swift
+++ BarcodeScanner/ScannerViewController.swift
@@ -5,4 +5,23 @@ protocol ScannerDelegate: AnyObject {
     func didFind(barcode: String)
 }
 
-final class ScannerViewController: UIViewController {}
+final class ScannerViewController: UIViewController {
+
+    // MARK: - Properties
+
+    private let captureSession = AVCaptureSession()
+    private var previewLayer: AVCaptureVideoPreviewLayer?
+    weak var scannerDelegate: ScannerDelegate?
+
+    // MARK: - Init
+
+    init(scannerDelegate: ScannerDelegate) {
+        super.init(nibName: nil, bundle: nil)
+        self.scannerDelegate = scannerDelegate
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
