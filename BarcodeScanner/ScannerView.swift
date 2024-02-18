fefb191cc27af28b5bed32f73e32850c789d7b88
Bruno Alves <alves.bruno945@gmail.com>
2024-02-18 17:31:07 -0300

    Add `ScannerView`

---
+++ BarcodeScanner/ScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  ScannerView.swift
+//  BarcodeScanner
+//
+//  Created by Bruno Silva on 18/02/24.
+//
+
+import SwiftUI
+
+struct ScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScannerView()
+}
